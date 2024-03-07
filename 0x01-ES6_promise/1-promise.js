c390950389d29330376870b886d3f2cfe627fe8e
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-07 18:45:04 +0200

    fixed some problems in file 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,12 @@
-export default function getFullResponseFromAPI(success: boolean) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                status: 200,
-                body: 'Success'
-            });
-        }
-        else {  
-            reject('The fake API is not working currently');
-        }
-    });
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else (false) {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
-
