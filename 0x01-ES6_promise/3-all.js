1c40a8c2d5ec45f742c8dbceb82ef0d7b05bb435
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-07 19:23:10 +0200

    fixed file 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,5 +5,7 @@ export default function handleProfileSignup() {
     .then((value) => {
       console.log('value[0].body, value[1].firstName, value[1].lastName');
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch((error) => {
+      console.log('Signup system offline');
+    });
 }
