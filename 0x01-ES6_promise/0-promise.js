eabe3dee1af5ea248f5b95548b495ec646362b09
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-07 18:34:04 +0200

    updated file 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('Response from the API');
-    }, 1000);
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
