ae1c50ee5153227893435ae10108491771cf6b2b
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-07 18:31:11 +0200

    add file utils.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,7 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve("Response from the API");
-      
+      resolve('Response from the API');
     }, 1000);
-});
+  });
 }
