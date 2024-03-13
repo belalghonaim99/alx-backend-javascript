b263c74aa707a105370273d53c1884e99dd10211
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-13 03:30:31 +0200

    fixed eslint

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,14 @@
 function updateStudentGradeByCity(students, city, newGrades) {
-    return students
-        .filter(student => student.location === city)
-        .map(student => {
-            const matchingGrade = newGrades.reduce((acc, curr) => {
-                return curr.studentId === student.id ? curr.grade : acc;
-            }, 'N/A');
-            return {
-                id: student.id,
-                firstName: student.firstName,
-                location: student.location,
-                grade: matchingGrade
-            };
-        });
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.reduce((acc, curr) => (curr.studentId === student.id ? curr.grade : acc), 'N/A');
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: matchingGrade,
+      };
+    });
 }
 export default updateStudentGradeByCity;
