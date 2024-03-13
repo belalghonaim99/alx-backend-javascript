43b07b7fb5693b0629ae3c9fc2a440a757991660
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-13 02:37:43 +0200

    fixed style

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,18 +2,17 @@ const getListStudents = () => [
   {
       id: 1,
       firstName: 'Guillaume',
-      location: 'San Francisco'
+      location: 'San Francisco',
   },
   {
       id: 2,
       firstName: 'James',
-      location: 'Columbia'
+      location: 'Columbia',
   },
   {
       id: 5,
       firstName: 'Serena',
-      location: 'San Francisco'
-  }
+      location: 'San Francisco',
+  },
 ];
-
 export default getListStudents;
