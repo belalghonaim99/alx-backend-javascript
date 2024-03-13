6e0dc4b7b8b2ffc3d80ff6072f5f8f919434253b
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-13 04:20:29 +0200

    updated

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
-const cleanSet = (set, startString) => { 
+const cleanSet = (set, startString) => {
   const filteredSet = new Set([...set].filter((value) => value.startsWith(startString)));
   return filteredSet;
-}; 
+};
 export default cleanSet;
