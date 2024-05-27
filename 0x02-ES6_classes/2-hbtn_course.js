cb38fded48643b03be0bf8805ca263df43ef960d
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-27 19:54:07 +0300

    updated file 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -5,8 +5,9 @@ class HolbertonCourse {
         this.students = students;
     }
 
+
     set name(name) {
-        if (typeof name !== 'String') {
+        if (typeof name !== 'string') {
         throw new TypeError('Name must be a string');
         }
         this._name = name;
@@ -17,17 +18,16 @@ class HolbertonCourse {
     }
 
     set length(length) {
-        if (typeof length !== 'Number') {
-        throw new TypeError('Length must be a number');
-    }
-    this._length = length;
+        if (typeof length !== 'number') {
+            throw new TypeError('Length must be a number');
+        }
+        this._length = length;
     }
 
     get length() {
         return this._length;
     }
-
-
+    
     set students(students) {
         if (students instanceof Array) {
         this._students = students;
