9b712ba21068448a8b672b4fe49d7ba8caa266f4
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-27 20:35:41 +0300

    updated file 100

--- 0x02-ES6_classes/100-main.js
+++ 0x02-ES6_classes/100-main.js
@@ -1,22 +1,7 @@
-import Car from './10-car';
+import EVCar from './100-evcar.js';
 
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this.range = range;
-  }
+const ec1 = new EVCar('Tesla', 'Turbo', 'Red', '250');
+console.log(ec1);
 
-  get range() {
-    return this._range;
-  }
-
-  set range(value) {
-    this._range = value;
-  }
-
-  cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
-  }
-}
+const ec2 = ec1.cloneCar();
+console.log(ec2);
