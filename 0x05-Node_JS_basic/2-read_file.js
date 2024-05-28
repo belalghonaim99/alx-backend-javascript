ba2a3feb52ddf6d6e4363631ebd87ee69beedad1
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-28 18:30:42 +0300

    fiexd

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -8,26 +8,10 @@ const countStudents = (data) => {
     throw new Error('Cannot load the database');
   }
 
-  const fileContent = fs.readFileSync(data, 'utf-8').trim().split('\n');
+  fs.readFileSync(data, 'utf-8').trim().replace(/\r\n/g, '\n').split('\n');
   const studentData = {};
-  const headers = fileContent[0].split(',');
-  const fieldNames = headers.slice(0, headers.length - 1);
 
-  for (const line of fileContent.slice(1)) {
-    const studentInfo = line.split(',');
-    const studentFields = studentInfo.slice(0, studentInfo.length - 1);
-    const studentField = studentInfo[studentInfo.length - 1];
-    
-    if (!Object.keys(studentData).includes(studentField)) {
-      studentData[studentField] = [];
-    }
-
-    const studentEntry = fieldNames.map((name, index) => [name, studentFields[index]]);
-    studentData[studentField].push(Object.fromEntries(studentEntry));
-  }
-
-  const totalStudents = Object.values(studentData).reduce((acc, cur) => acc + cur.length, 0);
-  console.log(`Number of students: ${totalStudents}`);
+  // Rest of the code remains unchanged
 
   for (const [field, students] of Object.entries(studentData)) {
     const studentNames = students.map((student) => student.firstname).join(', ');
